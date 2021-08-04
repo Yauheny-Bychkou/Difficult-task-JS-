49d4ffe634fc66e4bc9a7774e685f65fa26874af
Yauheny Bychkou <375445433672@yandex.by>
2021-08-04 15:42:35 +0300

    lesson05

--- script/script.js
+++ script/script.js
@@ -1,15 +1,16 @@
 "use strict";
-let str = "33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333";
+const chrs = ["2", "4"],
+  result = [10, 20, 30, 40, 50].filter((e) => chrs.includes(e.toString()[0]));
+console.log(result);
 
-const doString = function (a) {
-  if (typeof a === "string") {
-    a = a.trim();
-    if (a.length > 30) {
-      a = a.substr(0, 30) + "...";
+let n = 100;
+for (let i = 2; i <= n; i++) {
+  for (let j = 2; j <= i; j++) {
+    if (i % j === 0 && j !== i) {
+      break;
+    } else {
+      console.log(i);
+      break;
     }
-    return "Это строка:" + a;
-  } else {
-    return "Это не строка";
   }
-};
-console.log(doString(str));
+}
