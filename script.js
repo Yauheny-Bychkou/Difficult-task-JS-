833ca34cd6eff64a707e953a021391a7355af7c4
Yauheny Bychkou <375445433672@yandex.by>
2021-09-06 12:02:11 +0300

    filter

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
     cards = document.querySelector(".cards");
 
   const films = new Set();
+  const arr = [];
+  const newArr = [];
 
   const getData = (url) => {
     return new Promise((resolve, reject) => {
@@ -26,6 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const outputPhotos = (data) => {
     data.forEach((item) => {
+      arr.push(item);
       if (item.movies !== undefined) {
         item.movies.forEach((movie) => {
           films.add(movie);
@@ -35,21 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
     films.forEach((film) => {
       select.insertAdjacentHTML("beforeend", `<option value="${film}">${film}</option>`);
     });
-
-    data.forEach((item) => {
-      if (item.movies !== undefined) {
-        let arr = item.movies.join(", ");
-        cards.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card">
-          <img src="${item.photo}" alt="" class="photo" />
-          <h2><span>${item.name}</span></h2>
-          <h3 class="real-name">Real name: <span>${item.realName}</span></h3>
-          <h3 class="status">Status: <span>${item.status}</span></h3>
-          <h3>Movies: <span class="movies">${arr}</span></h3>
-        </div>`
-        );
-      } else {
+    const renderFunc = (data) => {
+      data.forEach((item) => {
         cards.insertAdjacentHTML(
           "beforeend",
           `<div class="card">
@@ -60,32 +50,24 @@ document.addEventListener("DOMContentLoaded", () => {
         <h3 class="movies">Movies: <span>${item.movies}</span></h3>
       </div>`
         );
-      }
-    });
-    const card = document.querySelectorAll(".card");
+      });
+    };
 
-    data.forEach((item) => {
-      if (item.movies !== undefined) {
-        item.movies.forEach((a) => {
-          select.addEventListener("change", () => {
-            if (select.value === a) {
-              let arr = item.movies.join(", ");
-              cards.innerHTML = "";
-              cards.insertAdjacentHTML(
-                "beforeend",
-                `<div class="card">
-                  <img src="${item.photo}" alt="" class="photo" />
-                  <h2><span>${item.name}</span></h2>
-                  <h3 class="real-name">Real name: <span>${item.realName}</span></h3>
-                  <h3 class="status">Status: <span>${item.status}</span></h3>
-                  <h3>Movies: <span class="movies">${arr}</span></h3>
-                </div>`
-              );
-            }
+    renderFunc(arr);
+    renderFunc(
+      arr.filter((item) => {
+        if (item.movies !== undefined) {
+          item.movies.forEach((film) => {
+            select.addEventListener("change", () => {
+              if (select.value === film) {
+                newArr.push(item);
+                console.log(item);
+              }
+            });
           });
-        });
-      }
-    });
+        }
+      })
+    );
   };
   const urlPhotos = "./dbHeroes.json";
   getData(urlPhotos)
